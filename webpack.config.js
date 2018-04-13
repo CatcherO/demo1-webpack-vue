3c696ee36eab2c7d5625cfd1a43144d29b47396d
liu <liuhui2583@qq.com>
2018-04-13 20:45:00 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path')
 
 module.exports = {
-    entrys: path.join(__dirname, 'src/index.js'),
+    entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename:'bundle.js',
-        path:path.join(__dirname,'dist')
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
 }
